598f175968f7284783a4bccad798fb7ddcf93098
ouxushengou <63182386+xushengou@users.noreply.github.com>
2024-05-20 22:15:09 -0700

    finished homepage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,8 +13,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _displayName = '';
-  List<String> dailyOptions = <String>["Awesome!", "It's Okay~", "Horriable!"];
-  var _dailyValue;
   List<String> quotes = <String>[
     "Believe you can and you're halfway there. - Theodore Roosevelt",
     "The only way to do great work is to love what you do. - Steve Jobs",
@@ -90,11 +88,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double boxDistance = (MediaQuery.of(context).size.width - 2*((MediaQuery.of(context).size.width - 20)/3))/6;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Padding(
-          padding: const EdgeInsets.only(left: 5.0),
+          padding: const EdgeInsets.only(left: 10.0),
           child: Row(
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -110,121 +109,304 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         physics: const ScrollPhysics(),
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(30.0),
           child: Column(
             children: [
+
+              // Four box of nav
               Column(
                 children: [
-                  const SizedBox(
-                    height: 15.0,
-                  ),
                   Align(
                     alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        DateTime.now().hour < 12
-                            ? "Good morning, $_displayName"
-                            : "Good afternoon, $_displayName",
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
+                    child: Text(
+                      DateTime.now().hour < 12
+                          ? "Good morning, $_displayName"
+                          : "Good afternoon, $_displayName",
+                      style: Theme.of(context).textTheme.headlineSmall,
                     ),
                   ),
-                  Container(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            boxWidget(
-                              title: "Goals",
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width - 20)/3,
+                        height: (MediaQuery.of(context).size.width - 20)/3,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                          ),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder:
+                                        (context, animation, secondaryAnimation) =>
+                                    const GratefulnessPage(),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      var begin = Offset(0.0, 1.0);
+                                      var end = Offset.zero;
+                                      var curve = Curves.ease;
+
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+
+                                      return SlideTransition(
+                                        position: animation.drive(tween),
+                                        child: child,
+                                      );
+                                    }),
+                              );
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              side: const BorderSide(
+                                color: Colors.transparent,
+                              ),
                             ),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
+                            child: Text(
+                              'Goals',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: boxDistance), // calculate the estimated width for the space between all four blocks, could use padding?
+
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width - 20)/3,
+                        height: (MediaQuery.of(context).size.width - 20)/3,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                          ),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder:
+                                        (context, animation, secondaryAnimation) =>
+                                    const GratefulnessPage(),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      var begin = Offset(0.0, 1.0);
+                                      var end = Offset.zero;
+                                      var curve = Curves.ease;
+
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+
+                                      return SlideTransition(
+                                        position: animation.drive(tween),
+                                        child: child,
+                                      );
+                                    }),
+                              );
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              side: const BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            child: Text(
+                              'To-dos',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: boxDistance),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width - 20)/3,
+                        height: (MediaQuery.of(context).size.width - 20)/3,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                          ),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder:
+                                        (context, animation, secondaryAnimation) =>
+                                    const GratefulnessPage(),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      var begin = Offset(0.0, 1.0);
+                                      var end = Offset.zero;
+                                      var curve = Curves.ease;
+
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+
+                                      return SlideTransition(
+                                        position: animation.drive(tween),
+                                        child: child,
+                                      );
+                                    }),
+                              );
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              side: const BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            child: Text(
+                              'Grateful...',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: boxDistance), // calculate the estimated width for the space between all four blocks, could use padding?
+
+                      SizedBox(
+                        width: (MediaQuery.of(context).size.width - 20)/3,
+                        height: (MediaQuery.of(context).size.width - 20)/3,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                          ),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder:
+                                        (context, animation, secondaryAnimation) =>
+                                    const GratefulnessPage(),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      var begin = Offset(0.0, 1.0);
+                                      var end = Offset.zero;
+                                      var curve = Curves.ease;
+
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+
+                                      return SlideTransition(
+                                        position: animation.drive(tween),
+                                        child: child,
+                                      );
+                                    }),
+                              );
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primary,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              side: const BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            child: Text(
+                              'Explore',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
               const SizedBox(
-                height: 10.0,
+                height: 30.0,
               ),
-              SizedBox(
-                width: double.maxFinite,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.background,
-                  ),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                            pageBuilder:
-                                (context, animation, secondaryAnimation) =>
-                                    const GratefulnessPage(),
-                            transitionsBuilder: (context, animation,
-                                secondaryAnimation, child) {
-                              var begin = Offset(1.0, 1.0);
-                              var end = Offset.zero;
-                              var curve = Curves.ease;
 
-                              var tween = Tween(begin: begin, end: end)
-                                  .chain(CurveTween(curve: curve));
-
-                              return SlideTransition(
-                                position: animation.drive(tween),
-                                child: child,
-                              );
-                            }),
-                      );
-                    },
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0)),
-                      side: const BorderSide(
-                        color: Colors.transparent,
-                      ),
-                    ),
+              // to-do list
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      'Complete your gratefulness',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.titleLarge,
+                      "Today",
+                      style: Theme.of(context).textTheme.headlineSmall,
                     ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(8),
+                      itemCount: dailyQuotes.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: 50,
+                          child: Text(
+                            '${index + 1}) ${dailyQuotes[index]}',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Text(
+              const SizedBox(
+                height: 30.0,
+              ),
+
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
                       "Quote of the day!",
-                      style: Theme.of(context).textTheme.titleLarge,
+                      style: Theme.of(context).textTheme.headlineSmall,
                     ),
-                    const SizedBox(
-                      height: 10.0,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(8),
+                      itemCount: dailyQuotes.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: 50,
+                          child: Text(
+                            '${index + 1}) ${dailyQuotes[index]}',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        );
+                      },
                     ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        padding: const EdgeInsets.all(8),
-                        itemCount: dailyQuotes.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return SizedBox(
-                            height: 50,
-                            child: Text(
-                              '${index + 1}) ${dailyQuotes[index]}',
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
